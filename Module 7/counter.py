f32d096acbb9c98e1e824d4cf1b1c2b548eea667
oluwarotimii <oluwarotimiadewumi@gmail.com>
2024-12-24 23:07:32 +0100

    module-7: the Summing Loop?, creating variabls adding the together in a loop

--- Module 7/counter.py
+++ Module 7/counter.py
@@ -14,3 +14,21 @@ print('This is the ', shaxan, 'time we are running this loop !')
 
 
 #this is an exmaple of a counting loop, I added my own modifications of previous examples into one
+
+
+# the Summing Loop
+
+zork = 0
+# zork is the counter fot the loop
+print ('The inception of this program', zork)
+for  samples in [23,434,34,5,45,6,5,67,78,7,89,8,9,56,56,56,56,3,7,834,767,45,56,23,65,23,756,23,56,23,56,234,6,23,56,76,23,56] :
+    #  we are going to be adding the loop number to the numbers
+    zork = zork + 1
+    print( 'Loop number is ', zork)
+    print('+++++++++++++++++++++++++++++')
+    zamps = zork + samples
+    print(zork, samples)
+    print('loop + samples', zamps)
+    # print(zork, samples)
+print('=====================================================================')
+print('final loop number', zork)
