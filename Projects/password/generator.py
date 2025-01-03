21301fd765aa9694d7ce605e7f11cb6c34ba5950
oluwarotimii <oluwarotimiadewumi@gmail.com>
2025-01-03 15:06:37 +0100

    projects: password generator

---
+++ Projects/password/generator.py
@@ -0,0 +1,12 @@
+# Password generator
+import random
+import string
+
+p = dict()
+def password_generator()  :
+    passwordLetters = string.ascii_letters
+    passwordLetters = passwordLetters.
+    print(passwordLetters)
+
+
+password_generator()
