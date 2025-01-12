77c891c1b5108e63b214794385ddab045f48e9dd
oluwarotimii <oluwarotimiadewumi@gmail.com>
2025-01-12 20:42:00 +0100

    proeject: the tdo list using a class and also trying to use the file persistence

--- Projects/To do list app/todo.py
+++ Projects/To do list app/todo.py
@@ -1,2 +1,29 @@
 #this is the To do list app
 #need to use OOP
+
+
+taskFile = open('Todo.txt', "w")
+class Tasks():
+    def __init__ (self, name, status):
+
+        self.name = name
+        self.status = status
+
+
+class TaskManager():
+    def __init__(self):
+        # taskFile = open('Todo.txt', "w")
+        self.tasks = open('Todo.txt', "w")
+
+    def addTasks(name, status):
+        taskName = input('Enter the Tasks: ')
+        taskStatus = input('Completed or Pending? : ')
+        name = taskName
+        status = taskStatus
+        taskFile.write(taskName)
+
+
+
+Todo = TaskManager(name, status)
+
+        
