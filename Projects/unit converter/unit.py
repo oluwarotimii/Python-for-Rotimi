92c5e070fa1b1e0d3d0f8be27d301416e1f234bd
oluwarotimii <oluwarotimiadewumi@gmail.com>
2025-01-21 19:45:53 +0100

    projects: trying the unit converter for different units

---
+++ Projects/unit converter/unit.py
@@ -0,0 +1,15 @@
+
+
+def converter():
+    options = ['Meters/Kilometer', 'Meters/Miles']
+    print(options)
+    print('Welcome to the Unit Converter. what you want to convert?')
+    prompt = input('What do you want to convert? : ') 
+    def meters(kilometers, miles) :
+        miles = 1106
+        kilometers = 1000
+    
+    # def calculate() :
+    #     if 
+
+converter()
